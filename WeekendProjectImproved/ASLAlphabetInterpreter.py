929dda42fc46b6987aae8dfceb94d45286888311
Nick Jackson <njackson2012@gmail.com>
2018-11-16 22:20:19 -0500

    Starting an improved interpreter

---
+++ WeekendProjectImproved/ASLAlphabetInterpreter.py
@@ -0,0 +1,11 @@
+class ASLAlphabetInterpreter(object):
+	def __init__(self):
+		self.workingDir = ""
+		self.handFinder = None
+		self.translater = None
+		self.lettersList = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N',
+	   'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
+
+	def train(self, set):
+		for s in set:
+			
---
+++ WeekendProjectImproved/FindHandModel.py
@@ -0,0 +1,2 @@
+class FindHandModel(object):
+	
