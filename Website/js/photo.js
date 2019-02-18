4141fc8df78a3cf64871a3162492982c217c5859
Joseph S. Lipinski <jsl94@drexel.edu>
2019-02-18 14:30:43 -0500

    Pushing updated website

---
+++ Website/js/photo.js
@@ -0,0 +1,28 @@
+(function (){
+  var video = document.getElementById('video'),
+      canvas = document.getElementById('canvas'),
+      context = canvas.getContext('2d'),
+      photo = document.getElementById('photo'),
+      vendorUrl = window.URL || window.webkitURL;
+  navigator.getMedia = navigator.getUserMedia ||
+                       navigator.webkitGetUserMedia ||
+                       navigator.mozGetUserMedia ||
+                       navigator.msGetUserMedia;
+                       
+  navigator.getMedia({
+    video: true,
+    audio: false
+  }, function(stream){
+    video.srcObject=stream;
+    video.play();
+  }, function(error){
+    //An error occured
+    //error.code
+  });
+
+  document.getElementById('capture').addEventListener('click', function() {
+    context.drawImage(video, 0, 0, 400, 300);
+    photo.setAttribute('src', canvas.toDataURL('image/png'));
+  });
+
+})();
