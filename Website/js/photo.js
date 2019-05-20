31ffe883c05fa4f1c5093fe8866d0bbffed19abd
Joseph S. Lipinski <jsl94@drexel.edu>
2019-05-20 15:06:29 -0400

    Saving local work

--- Website/js/photo.js
+++ Website/js/photo.js
@@ -14,15 +14,40 @@
     audio: false
   }, function(stream){
     video.srcObject=stream;
+
     video.play();
   }, function(error){
     //An error occured
     //error.code
   });
 
+  
   document.getElementById('capture').addEventListener('click', function() {
-    context.drawImage(video, 0, 0, 400, 300);
+    context.drawImage(video, 0, 0, 352, 228);
     photo.setAttribute('src', canvas.toDataURL('image/png'));
-  });
+    var data = canvas.toDataURL('image/png')
 
+    data = data.substring(22, data.length)
+    var xhttp = new XMLHttpRequest();
+    var url = "http://localhost:5010/processImage/"
+
+    var theUrl = "http://68.80.81.129:5010/addLetter/ Ok"
+
+    
+    var data = {"image":data}
+$.ajax({
+    type: 'POST',
+    contentType: 'application/json',
+    url: 'http:68.80.81.129:5010/processImage',
+    dataType : 'json',
+    data : JSON.stringify(data),
+    success : function(result) {
+      jQuery("#clash").html(result); 
+    },error : function(result){
+       console.log(result);
+    }
+});
+    
+  });
+  
 })();
